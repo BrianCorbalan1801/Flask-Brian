23539629f35393cb1f7dc5c79089f910a46de9e0
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-15 11:17:51 -0300

    Realize las 2 listas, la de actores y lenguaje

--- HVO+/__init__.py
+++ HVO+/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,3 +13,31 @@ def hello():
 @app.route('/quien')
 def quien():
     return 'Hecho por Brian Corbalan!'
+
+@app.route('/lenguaje')
+def idioma():
+    consulta = """
+        SELECT name FROM language  
+        ORDER BY name ASC
+    """
+
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_lenguaje = res.fetchall()
+    paginaLenguaje = render_template("lenguaje.html",
+                              lenguajes=lista_lenguaje)
+    return paginaLenguaje
+
+@app.route('/actor')
+def actor():
+    consulta = """
+        SELECT first_name, last_name FROM actor  
+        ORDER BY first_name, last_name ASC
+    """
+
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_actores = res.fetchall()
+    paginaActor = render_template("actor.html",
+                              actores=lista_actores)
+    return paginaActor
