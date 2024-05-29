f3c5d3299c765584bf78f890cad916856b3cf6fe
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-29 12:02:33 -0300

    Agregue el detalle de actores en el que se muestran las peliculas

--- HVO+/actores.py
+++ HVO+/actores.py
@@ -6,7 +6,7 @@ bp = Blueprint('actor', __name__, url_prefix="/actores")
 @bp.route('/')
 def actor():
     consulta = """
-        SELECT first_name, last_name FROM actor  
+        SELECT first_name, last_name, actor_id FROM actor  
         ORDER BY first_name, last_name ASC
     """
 
@@ -15,4 +15,27 @@ def actor():
     lista_actores = res.fetchall()
     paginaActor = render_template("actor.html",
                               actores=lista_actores)
+    return paginaActor
+
+@bp.route('/<int:id>')
+def detalle(id):
+    consulta = """
+        SELECT first_name, last_name FROM actor
+        WHERE actor_id = ?
+    """
+
+    con = db.get_db()
+    res = con.execute(consulta, (id,))
+    actor = res.fetchone()
+    consulta2 = """
+        SELECT title as titulo, f.film_id FROM film f
+        JOIN film_actor fil on f.film_id = fil.film_id
+        WHERE fil.actor_id = ?
+    """
+
+    res = con.execute(consulta2, (id,))
+    lista_peliculas = res.fetchall()
+    paginaActor = render_template("detalles_actor.html",
+                                  actor=actor,
+                                  peliculas=lista_peliculas)
     return paginaActor
